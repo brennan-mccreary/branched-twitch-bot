f1cfe6568310dcb0121865045564db8b20306817
Brennan McCreary <brennanmccreary@gmail.com>
2023-02-13 14:53:35 -0500

    Update command list and remove inundated features.

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const tmi = require('tmi.js');
 const blacklist = ['hello']
 
 //define commands
-const commands = {
-    website: {
-        response: 'http://twitch.tv/zeparatist'
-    },
-    upvote: {
-        response: (user) => `User ${user} was upvoted`
-    }
-}
+// const commands = {
+//     website: {
+//         response: 'http://twitch.tv/zeparatist'
+//     },
+//     upvote: {
+//         response: (user) => `User ${user} was upvoted`
+//     }
+// }
 
 //Define new tmi client
 const client = new tmi.Client({
     connection: {
         reconnect: true
     },
-    channels: [ 'Zeparatist' ],
+    channels: [ 'branchedgamingnetwork' ],
     identity: {
 		username: process.env.TWITCH_BOT_USERNAME,
 		password: process.env.TWITCH_OAUTH_TOKEN
@@ -35,57 +35,77 @@ client.connect();
 client.on('connected', (address, port) => {
     console.log(address)
     console.log(port)
-    //client.say(channel, "Now live!")
+    client.say(channel, "Now live!")
 })
 
-client.on('disconnected', (reason) => {
-    console.log(reason)
-    //client.say(channel, "End of stream!")
-})
+// client.on('disconnected', (reason) => {
+//     console.log(reason)
+//     //client.say(channel, "End of stream!")
+// })
 
 client.on('logon', (channel) => {
     console.log(channel)
 })
 
 
-client.on('join', (channel, username, self) => {
-    if(!self) client.say(channel, `Welcome, ${username}!`)
-})
+// client.on('join', (channel, username, self) => {
+//     if(!self) client.say(channel, `Welcome, ${username}!`)
+// })
 
-client.on('part', (channel, username, self) => {
-    if(!self) client.say(channel, `Goodbye, ${username}!`)
-})
+// client.on('part', (channel, username, self) => {
+//     if(!self) client.say(channel, `Goodbye, ${username}!`)
+// })
 
-client.on('raided', (channel, username, viewers) => {
-    client.say(channel, `${username} has raided with ${viewers}`)
-})
+// client.on('raided', (channel, username, viewers) => {
+//     client.say(channel, `${username} has raided with ${viewers}`)
+// })
 
-client.on('roomstate', (channel, state) => {
-    console.log(`Joined:${channel} ${state['room-id']}`)
-})
+// client.on('roomstate', (channel, state) => {
+//     console.log(`Joined:${channel} ${state['room-id']}`)
+// })
 
 
 
 
 //Message listener
 client.on('message', (channel, userstate, message, self) => {
+    //ignore bot sent messages
     if(userstate.username === process.env.TWITCH_BOT_USERNAME) return;
-    chatMod(userstate, message, channel);
+    
+    // //Auto mod features to get message id's and delete them if they contain blacklisted words
+    // chatMod(userstate, message, channel);
+    
+    //determine if message is a command    
     if(!message.startsWith('!')) return;
 
-    //Auto mod features to get message id's and delete them if they contain blacklisted words
+    //slice command keyword
 	const args = message.slice(1).split(' ');
 	const command = args.shift().toLowerCase();
 
-	if(command === 'echo') {
-		client.say(channel, `@${userstate.username}, you said: "${args.join(' ')}"`);
-	}
-    else if (command === 'bgn') {
-        client.say(channel, `https://discord.gg/tT2Vaqaj`)
+
+    //cross check command keywords
+	switch (command) {
+        case 'bgn':
+            client.say(channel, `We are sponsoring this event! \nJoin BGN: https://discord.gg/tT2Vaqaj`);
+            break;
+        case 'test':
+            console.log('test message for case 2');
+            break;
+        default:
+            console.log('No command found');
     }
-    else if (command === 'test') {
-        client.say(channel, `Watching: ${client.getUsername()}`)
-    }
+    
+    
+    // //Reference commands
+    // if(command === 'echo') {
+	// 	client.say(channel, `@${userstate.username}, you said: "${args.join(' ')}"`);
+	// }
+    // else if (command === 'bgn') {
+    //     client.say(channel, `https://discord.gg/tT2Vaqaj`)
+    // }
+    // else if (command === 'test') {
+    //     client.say(channel, `Watching: ${client.getUsername()}`)
+    // }
 });
 
 
